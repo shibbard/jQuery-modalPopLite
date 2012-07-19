b8b2b21278a9965d89a51f664adf743fcd6f0ed3
Simon Hibbard <hibbard.simon@gmail.com>
2012-07-19 11:31:41 +0100

    V1.31 release
    Added callback function ability.

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
 This is a super simple and lightweight jQuery Modal Popup dialog box.
 
-How to guide can be found here: http://www.tpddesign.co.uk/blog/modalpoplite/
+How to guide can be found here: http://www.mywebdeveloperblog.com/my-jquery-plugins/modalpoplite
 
-Demo can be found here: http://www.tpddesign.co.uk/projects/modalpoplite/demo/
+Demo can be found here: http://www.mywebdeveloperblog.com/projects/modalpoplite/demo/
 
--- modalPopLite.js
+++ modalPopLite.js
@@ -1,6 +1,6 @@
 ﻿/*
-* jQuery popBox
-* Copyright (c) 2010 Simon Hibbard
+* jQuery modalPopLite
+* Copyright (c) 2012 Simon Hibbard
 * 
 * Permission is hereby granted, free of charge, to any person
 * obtaining a copy of this software and associated documentation
@@ -25,44 +25,46 @@
 */
 
 /*
-* Version: V1.2.1
-* Release: 26-01-2011
-* Based on jQuery 1.4.2
-* Additional features provided with thanks to Alex Lareau
+* Version: V1.3.1
+* Release: 19-07-2012
+* Based on jQuery 1.7.2
 */
 
 (function ($) {
     var popID = 0;
     $.fn.modalPopLite = function (options) {
-        var options = $.extend({}, { openButton: "modalPopLite-open-btn", closeButton: "modalPopLite-close-btn" }, options);
+        var options = $.extend({}, { openButton: "modalPopLite-open-btn", closeButton: "modalPopLite-close-btn", isModal: false, callBack: null }, options);
 
         return this.each(function () {
             popID++;
             var thisPopID = popID;
+            var isOpen = false;
 
             obj = $(this);
             triggerObj = options.openButton;
             closeObj = options.closeButton;
+            isReallyModel = options.isModal;
 
             //alert("winH: " + winH + "top: " + top + "objH: " + objH);
             obj.before('<div id="modalPopLite-mask' + thisPopID + '" style="width:100%" class="modalPopLite-mask" />');
             obj.wrap('<div id="modalPopLite-wrapper' + thisPopID + '" style="left: -10000px;" class="modalPopLite-wrapper" />');
-
+            obj.addClass('modalPopLite-child-' + thisPopID);
 
             $(triggerObj).live("click", function (e) {
                 e.preventDefault();
                 var winW = $(window).width();
                 var winH = $(window).height();
-                var objW = obj.width();
-                var objH = obj.height();
+                var objW = $('.modalPopLite-child-' + thisPopID).outerWidth();
+                var objH = $('.modalPopLite-child-' + thisPopID).outerHeight();
                 var left = (winW / 2) - (objW / 2);
-                var top = (winH / 2) - (objH / 1.5);
+                var top = (winH / 2) - (objH / 2);
 
                 $('#modalPopLite-mask' + thisPopID).css('height', winH + "px");
                 $('#modalPopLite-mask' + thisPopID).fadeTo('slow', 0.6);
                 //$('#modalPopLite-wrapper' + thisPopID).hide();
-                $('#modalPopLite-wrapper' + thisPopID).css({'left': left + "px", 'top': top});
+                $('#modalPopLite-wrapper' + thisPopID).css({ 'left': left + "px", 'top': top });
                 $('#modalPopLite-wrapper' + thisPopID).fadeIn('slow');
+                isOpen = true;
             });
 
             $(closeObj).live("click", function (e) {
@@ -70,14 +72,35 @@
                 $('#modalPopLite-mask' + thisPopID).hide();
                 //$('#modalPopLite-wrapper' + thisPopID).hide();
                 $('#modalPopLite-wrapper' + thisPopID).css('left', "-10000px");
+                isOpen = false;
+                if (options.callBack != null) {
+                    options.callBack.call(this);
+                }
             });
 
             //if mask is clicked
-            $('#modalPopLite-mask' + thisPopID).click(function (e) {
-                e.preventDefault();
-                $(this).hide();
-                //$('#modalPopLite-wrapper' + thisPopID).hide();
-                $('#modalPopLite-wrapper' + thisPopID).css('left', "-10000px");
+            if (!isReallyModel) {
+                $('#modalPopLite-mask' + thisPopID).click(function (e) {
+                    e.preventDefault();
+                    $(this).hide();
+                    //$('#modalPopLite-wrapper' + thisPopID).hide();
+                    $('#modalPopLite-wrapper' + thisPopID).css('left', "-10000px");
+                    isOpen = false;
+                    if (options.callBack != null) {
+                        options.callBack.call(this);
+                    }
+                });
+            }
+            $(window).resize(function () {
+                if (isOpen) {
+                    var winW = $(window).width();
+                    var winH = $(window).height();
+                    var objW = $('.modalPopLite-child-' + thisPopID).outerWidth();
+                    var objH = $('.modalPopLite-child-' + thisPopID).outerHeight();
+                    var left = (winW / 2) - (objW / 2);
+                    var top = (winH / 2) - (objH / 2);
+                    $('#modalPopLite-wrapper' + thisPopID).css({ 'left': left + "px", 'top': top });
+                }
             });
         });
 
@@ -87,6 +110,9 @@
         $('#modalPopLite-mask' + id).hide();
         //$('#modalPopLite-wrapper' + id).hide();
         $('#modalPopLite-wrapper' + thisPopID).css('left', "-10000px");
+        if (options.callBack != null) {
+            options.callBack.call(this);
+        }
     };
 
     $.fn.modalPopLite.ShowProgress = function () {
